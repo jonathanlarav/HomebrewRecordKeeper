28e57b73f1716d85692fab32282ce84006db5653
MindD3v <juanlorenzo.hinojosa@gmail.com>
2014-02-21 14:31:39 -0700

    Test class works

---
+++ src/test/java/com/homebrewrecordkeeper/mvc/AppTests.java
@@ -0,0 +1,11 @@
+package com.homebrewrecordkeeper.mvc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jhinojosa
+ * Date: 2/21/14
+ * Time: 1:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppTests {
+}
